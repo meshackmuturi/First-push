a7d4a0d57d09308f2bedc885fbcc06891b4414c2
Meshack Muturi <mwalumameshack@gmail.com>
2025-06-17 22:39:10 +0300

    initial commit

---
+++ Perimeter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int L;
+    int W;
+    cout <<" Enter length"<<endl;
+    cin >>L;
+    cout <<"Enter Width"<<endl;
+    cin>> W;
+    int sum = 2*(L+W);
+    cout << sum<< endl;
+    return 0;
+}
