e6c7d49bc605242b6992d62ca433c4a22ecb42e1
meshackmuturi <mwalumameshack@gmail.com>
2025-06-23 22:29:18 +0300

    Add files via upload
    else function C

---
+++ Else Function.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	/*local variable defination*/
+	int a=50;
+	if(a<20)
+printf("%d is less than 20\n",a);
+	
+	else
+	printf("%d is greater than or equal to 20\n",a);
+	return 0;
+	
+}
